8f1758c38839f9e631450618d16af942f84932ae
Ubuntu Desktop <vanyoq@gmail.com>
2025-08-28 15:02:31 +0300

    New commit

--- scripts/ssh-key.sh
+++
@@ -1,2 +0,0 @@
-sudo -u mqttapp ssh-keygen -t ed25519 -C "mqttapp@server" -f /opt/mqttapp/.ssh/id_ed25519 -N ""
-sudo cat /opt/mqttapp/.ssh/id_ed25519.pub
--- scripts/user-and-dir-install.sh
+++ scripts/user-and-dir-install.sh
@@ -1,4 +1,4 @@
 sudo adduser --system --group --home /opt/mqttapp mqttapp
-sudo -u mqttapp mkdir -p /opt/mqttapp/{app,logs,data
+sudo mkdir -p /opt/mqttapp/{app,logs,data,scripts}
 sudo chown -R mqttapp:mqttapp /opt/mqttapp
 sudo -u mqttapp chmod -R 755 /opt/mqttapp
